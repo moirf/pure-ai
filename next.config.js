0589e2464483adde73dfb8d3a3d6120a6ad99849
Mohammad Irfan (HCL Technologies Corporate Services) <v-irfanmo@microsoft.com>
2025-08-11 12:08:00 -0700

    Fix the path issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'pure-ai';
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true
   }
